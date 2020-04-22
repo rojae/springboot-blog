c61ca8be62949c94d3e53b24bd24f22da39f7fab
rojae <Jae.Thon96@gmail.com>
2020-04-22 23:33:11 +0900

    Initial commit

---
+++ src/main/java/com/rojae/blog/HelloRestController.java
@@ -0,0 +1,13 @@
+package com.rojae.blog;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+    @RequestMapping("/")
+    public String index() {
+        return "helloworld!";
+    }
+}
+
---
+++ src/main/java/com/rojae/blog/SpringbootBlogApplication.java
@@ -0,0 +1,13 @@
+package com.rojae.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBlogApplication.class, args);
+    }
+
+}
